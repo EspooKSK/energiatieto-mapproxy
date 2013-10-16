4bc2d05d5701eef843b79c747b489aba89fbf621
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2013-10-16 15:17:09 +0300

    move wms url to be configured via environment variables

--- server.py
+++ server.py
@@ -1,2 +1,16 @@
 from mapproxy.wsgiapp import make_wsgi_app
+from string import Template
+import os
+
+# render config template from environment variables
+
+template_file = open('mapproxy.yaml.tmpl', 'r')
+template = Template(template_file.read())
+template_file.close()
+output = template.substitute(wms_url=os.environ['WMS_URL'])
+
+target_file = open('mapproxy.yaml', 'w')
+target_file.write(output)
+target_file.close()
+
 application = make_wsgi_app('mapproxy.yaml')
